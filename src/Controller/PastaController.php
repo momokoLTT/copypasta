ced44f98d2433efbf284e10c7af78c11f0eb64f3
MomokoLTT <momoko.latoto@gmail.com>
2022-09-26 03:37:20 +0200

    doctrine got re-added by accident; is now re-removed

--- src/Controller/PastaController.php
+++ src/Controller/PastaController.php
@@ -12,30 +12,25 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route(path="/pasta")
- */
 class PastaController extends AbstractController
 {
     public function __construct(private PastaService $service)
     {
     }
 
-    /**
-     * @Route(path="/you-know-what")
-     *
-     * @throws JsonException
-     */
+    #[Route(path: '/pasta/you-know-what', methods: ['GET', 'POST'])]
     public function youKnowWhat(Request $request): Response
     {
-        $input = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        try {
+            $input = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        } catch (JsonException) {
+            return new Response('An error occurred while attempting to parse your input');
+        }
 
         return new Response($this->service->createPasta(YouKnowWhatData::KEY, $input));
     }
 
-    /**
-     * @Route(path="/test")
-     */
+    #[Route(path: '/pasta/test', methods: ['GET', 'POST'])]
     public function test(): Response
     {
         return new Response($this->service->createPasta(YouKnowWhatData::KEY, [
